36e56c2d3a4a0d1592c5f8bc68533453f8c777d8
Shane_Park <kkobuk@hotmail.co.kr>
2021-01-19 20:32:29 +0900

    pdfBox test first commit

---
+++ src/test/PdfBoxTest.java
@@ -0,0 +1,28 @@
+package test;
+
+
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+
+public class PdfBoxTest {
+
+	public static void main(String[] args) {
+		createEmptyDocument();
+	}
+
+	private static void createEmptyDocument() {
+		try (final PDDocument doc = new PDDocument()) {
+			final PDPage emptyPage = new PDPage();
+			doc.addPage(emptyPage);
+
+			doc.save("testEmptyPage.pdf");
+			System.out.println("Created successfully!");
+		} catch (IOException e) {
+			System.out.println("Fail to create PDF while trying to create blank document - " + e);
+		}
+
+	}
+
+}
