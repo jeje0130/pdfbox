fac195ffd8366488046ca9b7592cb0bd737a7e1a
pamlpri <kid03196@gmail.com>
2021-01-21 18:17:10 +0900

    PDFBOX_주석추가

--- src/pdfbox/PdfBoxWriteText_Test.java
+++ src/pdfbox/PdfBoxWriteText_Test.java
@@ -1,5 +1,4 @@
-package pdfbox;
-
+package test;
 import java.io.IOException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -7,42 +6,51 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
-public class PdfBoxWriteText_Test {
+public class PdfBoxWriteText_Test {	// PDFBOX를 이용해 PDF파일을 만드는 예제입니다.
 
    public static void main(String[] args) throws IOException {
-         // 새로운 document 생성
-      try (PDDocument doc = new PDDocument()) {
+         // 1) 새로운 PDDocument 객체를 생성 합니다
+      try (PDDocument doc = new PDDocument()) {	
          
-         // 생성한 document 사용
+         // 2) 새로운 PDF 페이지 객체를 생성 합니다.
          PDPage myPage = new PDPage();
+         
+         // 3) 새로운 PDF페이지를 생성합니다.
          doc.addPage(myPage);
          
-         try (PDPageContentStream cont = new PDPageContentStream(doc, myPage)) {
-            // 글쓰기 내용 시작
+         // 4) PDF 파일에 글을 입력하기 위해선 PDPageContentStream 객체를 생성합니다.
+         try(PDPageContentStream cont = new PDPageContentStream(doc, myPage)) {
+            // 5) 글쓰기 내용 시작점 입니다.
             cont.beginText();
             
-            // 글씨 폰트 설정
+            // 6) 글씨 폰트 설정합니다.
             cont.setFont(PDType1Font.TIMES_ROMAN, 12);
             
-            // ????
-            cont.setLeading(14.5f);
-            cont.newLineAtOffset(25, 700);
+            // 7) 글의 줄 간격을 정해줍니다.
+            cont.setLeading(50.5f);
+            
+            // 8) 기준점이 왼쪽 하단 모서리를 기점으로(X,Y) 만큼 시작 위치가 변경됩니다.
+            cont.newLineAtOffset(25, 700);	// 라인 위치            
+            
+            // 9) 문서의 내용을 첨부합니다.
             String line1 = "Hello JAVA!!";
             
-            // 입력 보여주기
+            // 10) 문서 내용을 보여줍니다.
             cont.showText(line1);
             
-            // 줄 변경
+            // 11) 줄을 변경합니다.
             cont.newLine();
             
             String line2 = "Hello PDFBox!!";
             cont.showText(line2);
-                cont.newLine();
-                
-                cont.endText();
-         }
-         
-         doc.save("PdfBoxWriteText_Test.pdf");
+            
+            cont.newLine();
+            
+            System.out.println("PDF 파일 생성 완료");
+            cont.endText();	// 11) 문서를 닫습니다.        	 
+         }      
+         //12) PDF 파일명을 지정합니다.
+         doc.save("PdfBoxWriteText.pdf");
       }
 
    }
